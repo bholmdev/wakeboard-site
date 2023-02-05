28e2e1a69121bd45ca91e8f7104ae5c19856c627
Brandon Holman <bholmdev@gmail.com>
2023-02-05 11:04:29 -0600

    Banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import logo from "./app/assets/img/logo.jpg";
 
 function App() {
   return (
     <div className="App">
+      <Navbar dark color="primary" stick="top" expand="md">
+        <Container>
+          <NavbarBrand href="/">
+            <img src={logo} alt="logo" />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
       You Can Wakeboard Too!
     </div>
   );
