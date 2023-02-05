9fe67712ec7ebc107376bf9b393f8d8ee231f620
Brandon Holman <bholmdev@gmail.com>
2023-02-05 16:11:21 -0600

    Header and Footer Links

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,4 +1,5 @@
 import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
@@ -9,7 +10,21 @@ const Footer = () => {
                         xs={{ size: 4, offset: 1 }}
                         sm="2"
                     >
-                        <h5>This will be site navigation</h5>
+                        <h5>Links</h5>
+                        <ul className="list-unstyled">
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/directory">Directory</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/contact">Contact</Link>
+                            </li>
+                        </ul>
                     </Col>
                     <Col
                         xs="6" sm="3"
--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,12 +1,53 @@
-import { Navbar, NavbarBrand } from "reactstrap";
-import logo from "../assets/img/logo.jpg";
+import { useState } from "react";
+import {
+    Collapse,
+    Nav,
+    Navbar,
+    NavbarBrand,
+    NavbarToggler,
+    NavItem
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
+import logo from "../assets/img/logo.png";
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
-        <Navbar dark color="primary" stick="top" expand="md">
-            <NavbarBrand href="/">
-                <img src={logo} alt="logo" />
+        <Navbar
+            dark color="primary"
+            stick="top"
+            expand="md"
+        >
+            <NavbarBrand className="ms-5" href="/">
+                <img src={logo} alt="logo" className="float-start" />
+                <h1 className="mt-2">Wakeboard United</h1>
             </NavbarBrand>
+            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+            <Collapse isOpen={!menuOpen} navbar>
+                <Nav className="ms-auto" navbar>
+                    <NavItem>
+                        <NavLink className="nav-link" to="/">
+                            <i className="fa fa-home fa-lg" /> Home
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className="nav-link" to="/directory">
+                            <i className="fa fa-list fa-lg" /> Directory
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className="nav-link" to="/about">
+                            <i className="fa fa-info fa-lg" /> About
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className="nav-link" to="/contact">
+                            <i className="fa fa-address fa-lg" /> Contact
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
         </Navbar>
     );
 };
