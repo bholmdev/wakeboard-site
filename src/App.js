67283534e53d197dda0801240cc675c617fc0ec0
Brandon Holman <bholmdev@gmail.com>
2023-02-05 15:44:16 -0600

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Header from './app/components/Header';
+import HomePage from './app/pages/HomePage';
 import ParksDirectoryPage from './app/pages/ParksDirectoryPage';
+import ContactPage from './app/pages/ContactPage';
 import Footer from './app/components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ParksDirectoryPage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<ParksDirectoryPage />} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-social/bootstrap-social.css';
 import 'font-awesome/css/font-awesome.css';
@@ -18,7 +18,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
