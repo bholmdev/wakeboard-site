b3848d23d9002ed413b341f7475036ac164f2b40
Brandon Holman <bholmdev@gmail.com>
2023-02-05 10:38:23 -0600

    Added Parks Data

---
+++ src/app/shared/PARKS.js
@@ -0,0 +1,79 @@
+import texasskiranch from "../assets/img/texasskiranch.jpg";
+import velocityislandpark from "../assets/img/velocityislandpark.jpg";
+import thelagoon from "../assets/img/thelagoon.jpg";
+import thespin from "../assets/img/thespin.jpg";
+import wakeparkthulba from "../assets/img/wakeparkthulba.jpg";
+import singaporewake from "../assets/img/singaporewake.jpg"
+
+
+
+export const PARKS = [
+    {
+        id: 0,
+        name: "Texas Ski Ranch",
+        image: texasskiranch,
+        alt: "Texas Ski Ranch",
+        featured: true,
+        description:
+            "No boat? No problem! Wakeboard and waterski on the cable lakes at TSR. We pride ourselves on having one of the top ranked wakeboarding facilities in the world with some of the best coaches and southern hospitality that can't be beat.",
+        country: "USA",
+        city: "",
+        state: "",
+        link: "https://www.texasskiranch.com/"
+    },
+    {
+        id: 1,
+        name: 'Velocity Island Park',
+        image: velocityislandpark,
+        featured: false,
+        description: "Experience our state-of-the-art cable towing system. The perfect learning tool for beginners and the most fun for our advanced riders trying new tricks on our features.",
+        country: "USA",
+        city: "New Braunfels",
+        state: "Texas",
+        link: "https://www.velocityislandpark.com/"
+    },
+    {
+        id: 2,
+        name: "The Lagoon",
+        image: thelagoon,
+        featured: false,
+        description: "Founded in 2011 and reimagined in 2020 The Lagoon on Grassy Key has been a middle Keys staple for nearly 10 years.  Keys Cable Park was created for teaching locals and visitors alike to enjoy a variety of board, wind, and paddle sports.",
+        country: "USA",
+        city: "Marathon",
+        state: "Florida",
+        link: "https://ridethelagoon.com/"
+    },
+    {
+        id: 3,
+        name: "The Spin",
+        image: thespin,
+        featured: true,
+        description: "The Spin cablepark, the big cable or the 5.0, is a 620m loop. There are up to 25 people at the same time who can indulge in the joys of skiing! In recent years, the water ski lift has evolved to adapt to the practices of wakeboarding and wakeskating.",
+        country: "Belgium",
+        city: "Froidchapelle",
+        state: "Hainaut",
+        link: "https://thespin.be"
+    },
+    {
+        id: 4,
+        name: "Wake Park Thulba",
+        image: wakeparkthulba,
+        featured: false,
+        description: "Are you looking for sport, fun and nature? Are you looking for the right mix of chill & thrill? Then we warmly welcome you to WakePark Thulba, the center of fun and action in the Thulba Valley.",
+        country: "Germany",
+        city: "Oberthulba",
+        state: "Bavaria",
+        link: "https://www.wakepark-thulba.de/"
+    },
+    {
+        id: 5,
+        name: "Singapore Wake Park",
+        image: singaporewake,
+        featured: true,
+        description: "Singapore Wake Park is the country's only cable-ski park built in a lagoon at Singapore East Coast Park. It ensures a safe environment for both beginners and experienced riders looking for an adrenaline fix.",
+        country: "Singapore",
+        city: "Katong",
+        state: "East Region",
+        link: "https://singaporewakepark.com/"
+    },
+];
---
+++ src/app/shared/PARTNERS.js
@@ -0,0 +1,36 @@
+
+
+export const PARTNERS = [
+    {
+        id: 0,
+        name: 'Bootstrap Outfitters',
+        image: bootStrapLogo,
+        featured: false,
+        description:
+            "Bootstrap Outfitters supplies you with the gear you need at prices you can't beat."
+    },
+    {
+        id: 1,
+        name: 'Git Out Expeditions',
+        image: gitLogo,
+        featured: false,
+        description:
+            'Join Git Out Expeditions to explore new horizons with a group of other adventurers!'
+    },
+    {
+        id: 2,
+        name: 'Mongo Fly Shop',
+        image: mongoLogo,
+        featured: false,
+        description:
+            'Need a new fishing pole, a tacklebox, or flies of all kinds? Stop by Mongo Fly Shop.'
+    },
+    {
+        id: 3,
+        name: 'Node Outdoor Apparel',
+        image: nodeLogo,
+        featured: true,
+        description:
+            'From polar fleeces to swimsuits, hiking boots to waders, a visit to Node will be sure to get you covered.'
+    }
+];
