4cb51b00578634f6a30922e1f1b739d411df17d1
Brandon <112407387+bholmdev@users.noreply.github.com>
2024-08-31 17:58:20 -0500

    added basename to browswerrouter

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="{ /${process.env.PUBLIC_URL}}">
         <App />
       </BrowserRouter>
     </Provider>
